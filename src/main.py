945a9a7cc050b579e58e8aca68206359f02c1cac
adityagoyal <goyaladitya85@gmail.com>
2025-07-22 12:05:05 +0200

    changes in naive bayes and transformer

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from classifier import predict_news
 
 app = FastAPI(title="Game Recommender")
 
+# simple get request to predict the classification of an article
 @app.get("/predict")
 def predict(text: str = Query(..., description="Enter the text for prediction")):
     prediction, confidence = predict_news(text)
